4fe3c5105d2340ef84bbe074b0ab3d9dec28797f
Bob van Mierlo <38190383+bobvmierlo@users.noreply.github.com>
2025-07-30 14:51:19 +0200

    Update dmwl_client.py
    Added more attributes to the dataset to get more DICOM tags in the detailed result

--- dmwl_client.py
+++ dmwl_client.py
@@ -3,24 +3,70 @@ from pynetdicom import AE
 from pynetdicom.sop_class import ModalityWorklistInformationFind
 
 def query_worklist(query_data, calling_ae_title, called_ae_title, server_ip, server_port):
-    # Create AE with calling AE title
     ae = AE(ae_title=calling_ae_title)
     ae.add_requested_context(ModalityWorklistInformationFind)
 
     ds = Dataset()
-    # Wildcard around PatientName for "contains" match
-    patient_name = query_data.get('PatientName', '').strip()
-    ds.PatientName = f"*{patient_name}*" if patient_name else '*'
+
+    # Patient-related attributes
+    ds.PatientName = f"*{query_data.get('PatientName', '').strip()}*" if query_data.get('PatientName') else '*'
     ds.PatientID = query_data.get('PatientID', '')
     ds.AccessionNumber = query_data.get('AccessionNumber', '')
+    ds.PatientBirthDate = ''
+    ds.PatientSex = ''
+    ds.PatientSize = ''
+    ds.PatientWeight = ''
+    ds.PregnancyStatus = ''
+    ds.MedicalAlerts = ''
+    ds.Allergies = ''
+    ds.PatientComments = ''
+    ds.IssuerOfPatientID = ''
 
+    # Study/Request-related attributes
+    ds.RequestedProcedureID = ''
+    ds.RequestedProcedureDescription = ''
+    ds.RequestedProcedurePriority = ''
+    ds.ConfidentialityConstraintOnPatientDataDescription = ''
+    ds.ReferringPhysicianName = ''
+    ds.RequestingPhysician = ''
+    ds.StudyInstanceUID = ''
+
+    # Visit-related attributes
+    ds.AdmissionID = ''
+    ds.SpecialNeeds = ''
+    ds.CurrentPatientLocation = ''
+    ds.PatientState = ''
+
+    # Referenced study (optional)
+    referenced_study = Dataset()
+    referenced_study.ReferencedSOPClassUID = ''
+    referenced_study.ReferencedSOPInstanceUID = ''
+    ds.ReferencedStudySequence = [referenced_study]
+
+    # Specific character set (to avoid encoding issues)
+    ds.SpecificCharacterSet = 'ISO_IR 100'
+
+    # Scheduled Procedure Step Sequence
     sps_item = Dataset()
     sps_item.Modality = query_data.get('Modality', '')
     sps_item.ScheduledStationAETitle = query_data.get('StationAETitle', '')
     sps_item.ScheduledProcedureStepStartDate = query_data.get('StartDate', '')
+    sps_item.ScheduledProcedureStepStartTime = ''
+    sps_item.ScheduledPerformingPhysicianName = ''
+    sps_item.ScheduledProcedureStepDescription = ''
+    sps_item.ScheduledProcedureStepID = ''
+    sps_item.ScheduledStationName = ''
+    sps_item.ScheduledProcedureStepLocation = ''
+
+    # Protocol Code Sequence inside SPS
+    protocol_code_item = Dataset()
+    protocol_code_item.CodeValue = ''
+    protocol_code_item.CodingSchemeDesignator = ''
+    protocol_code_item.CodeMeaning = ''
+    sps_item.ScheduledProtocolCodeSequence = [protocol_code_item]
+
     ds.ScheduledProcedureStepSequence = [sps_item]
 
-    # Here the called AE title (server's AE) is specified
     assoc = ae.associate(server_ip, server_port, ae_title=called_ae_title)
 
     results = []
